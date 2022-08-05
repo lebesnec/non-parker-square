d4743bf667409c493b1a9969637a83923764e0a4
Christophe Le Besnerais <christophe.lebesnerais@gmail.com>
2022-08-05 21:38:18 +0200

    printing

--- non-parker-square.js
+++ non-parker-square.js
@@ -4,18 +4,26 @@
 //  --|---|--
 //  g | h | i
 
-function formatNumber(x) {
-    return ('' + x).padStart(4, ' ');
+function format(x, padLength) {
+    return ('' + x).padStart(padLength, ' ');
 }
 
 function printSquare(square) {
     console.log(`
-        ${formatNumber(square.a)} | ${formatNumber(square.b)} | ${formatNumber(square.c)}
-        -----|------|-----
-        ${formatNumber(square.d)} | ${formatNumber(square.e)} | ${formatNumber(square.f)}
-        -----|------|-----
-        ${formatNumber(square.g)} | ${formatNumber(square.h)} | ${formatNumber(square.i)}
-         Total is ${square.total}
+        ${format(square.a, 4)}² | ${format(square.b, 4)}² | ${format(square.c, 4)}²     ${format(square.a ** 2, 7)} | ${format(square.b ** 2, 7)} | ${format(square.c ** 2, 7)}
+        ------|-------|------     --------|---------|--------
+        ${format(square.d, 4)}² | ${format(square.e, 4)}² | ${format(square.f, 4)}²     ${format(square.d ** 2, 7)} | ${format(square.e ** 2, 7)} | ${format(square.f ** 2, 7)}
+        ------|-------|------     --------|---------|--------
+        ${format(square.g, 4)}² | ${format(square.h, 4)}² | ${format(square.i, 4)}²     ${format(square.g ** 2, 7)} | ${format(square.h ** 2, 7)} | ${format(square.i ** 2, 7)}
+
+        a² + b² + c² = ${square.a}² + ${square.b}² + ${square.c}² = ${square.a ** 2} + ${square.b ** 2} + ${square.c ** 2} = ${(square.a ** 2) + (square.b ** 2) + (square.c ** 2)}
+        d² + e² + f² = ${square.d}² + ${square.e}² + ${square.f}² = ${square.d ** 2} + ${square.e ** 2} + ${square.f ** 2} = ${(square.d ** 2) + (square.e ** 2) + (square.f ** 2)}
+        g² + h² + i² = ${square.g}² + ${square.h}² + ${square.i}² = ${square.g ** 2} + ${square.h ** 2} + ${square.i ** 2} = ${(square.g ** 2) + (square.h ** 2) + (square.i ** 2)}
+        a² + d² + g² = ${square.a}² + ${square.d}² + ${square.g}² = ${square.a ** 2} + ${square.d ** 2} + ${square.g ** 2} = ${(square.a ** 2) + (square.d ** 2) + (square.g ** 2)}
+        b² + e² + h² = ${square.b}² + ${square.e}² + ${square.h}² = ${square.b ** 2} + ${square.e ** 2} + ${square.h ** 2} = ${(square.b ** 2) + (square.e ** 2) + (square.h ** 2)}
+        c² + f² + i² = ${square.c}² + ${square.f}² + ${square.i}² = ${square.c ** 2} + ${square.f ** 2} + ${square.i ** 2} = ${(square.c ** 2) + (square.f ** 2) + (square.i ** 2)}
+        a² + e² + i² = ${square.a}² + ${square.e}² + ${square.i}² = ${square.a ** 2} + ${square.e ** 2} + ${square.i ** 2} = ${(square.a ** 2) + (square.e ** 2) + (square.i ** 2)}
+        g² + e² + c² = ${square.g}² + ${square.e}² + ${square.c}² = ${square.g ** 2} + ${square.e ** 2} + ${square.c ** 2} = ${(square.g ** 2) + (square.e ** 2) + (square.c ** 2)}
     `);
 }
 
@@ -85,6 +93,62 @@ console.log('Searching up to ' + LIMIT + '...');
 console.time('Duration');
 
 // try every combination possible for the 4 values a, b, c, d
+
+// for (let a = 0; a <= LIMIT; a++) {
+//     const aSquare = a ** 2;
+//     for (let b = 0; b <= LIMIT; b++) {
+//         if (a === b) {
+//             continue;
+//         };
+//         const bSquare = b ** 2;
+//         for (let c = 0; c <= LIMIT; c++) {
+//             if (a === c || b === c) {
+//                 continue;
+//             };
+//             const cSquare = c ** 2;
+//             const total = aSquare + bSquare + cSquare;
+//             for (let d = 0; d <= LIMIT; d++) {
+//                 if (a === d || b === d || c === d) {
+//                     continue;
+//                 };
+//                 const dSquare = d ** 2;
+//                 const gSquare = total - dSquare - aSquare;
+//                 const g = ROOTS[gSquare];
+//                 if (g === undefined) {
+//                     continue;
+//                 }
+//                 const eSquare = total - gSquare - cSquare;
+//                 const e = ROOTS[eSquare];
+//                 if (e === undefined) {
+//                     continue;
+//                 }
+//                 const hSquare = total - eSquare - bSquare;
+//                 const h = ROOTS[hSquare];
+//                 if (h === undefined) {
+//                     continue;
+//                 }
+//                 const fSquare = total - dSquare - eSquare;
+//                 const f = ROOTS[fSquare];
+//                 if (f === undefined) {
+//                     continue;
+//                 }
+//                 const iSquare = total - gSquare - hSquare;
+//                 const i = ROOTS[iSquare];
+//                 if (i === undefined) {
+//                     continue;
+//                 }
+//                 squares.push({
+//                     a, b, c,
+//                     d, e, f,
+//                     g, h, i,
+//                     total
+//                 });
+//             }
+//         }
+//     }
+// }
+
+
 for (let c = 0; c <= LIMIT; c++) {
     const cSquare = c ** 2;
     for (let d = 0; d <= LIMIT; d++) {
@@ -131,8 +195,7 @@ for (let c = 0; c <= LIMIT; c++) {
                     squares.push({
                         a, b, c,
                         d, e, f,
-                        g, h, i,
-                        total
+                        g, h, i
                     });
                 }
             }
